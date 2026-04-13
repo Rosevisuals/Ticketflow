c4171bcd1f9eba3a2bd96c45e9d2bedfc102f010
Namubiru Rose <145221322+Rosevisuals@users.noreply.github.com>
2026-04-13 12:02:21 +0300

    Initial Commit

---
+++ Ticketflow/TicketflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketflowApp.swift
+//  Ticketflow
+//
+//  Created by Rose Visuals on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct TicketflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
